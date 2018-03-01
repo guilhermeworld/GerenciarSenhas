784a334109ebda7ca1a4c0419840782d2e2f1ac8
Guilherme Oliveira <guilhermeoliveira-@hotmail.com>
2018-03-01 14:28:30 -0300

    Revsão

---
+++ GerenciarSenhas/Controllers/ContaController.cs
@@ -0,0 +1,92 @@
+﻿using GerenciarSenhas.Models;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace GerenciarSenhas.Controllers
+{
+	public class ContaController : Controller
+    {
+		[AllowAnonymous]
+		public ActionResult Login()
+		{
+			if (User.Identity.IsAuthenticated)
+			{
+				return RedirectToAction("Index", "Senhas");
+			}
+			return View();
+		}
+
+		[HttpPost]
+		[AllowAnonymous]
+		public ActionResult Login(Usuario usuario)
+		{
+			
+			if (!ModelState.IsValid)
+			{
+				return View();
+			}
+
+			using (GerenciaSenhasEntities bd = new GerenciaSenhasEntities())
+			{
+				var v = bd.Usuario.Where(a => a.Login.Equals(usuario.Login) && a.Senha.Equals(usuario.Senha)).FirstOrDefault();
+				if (v != null)
+				{
+					Session.Add("ID_usuario", v.ID);
+					FormsAuthentication.SetAuthCookie(usuario.Login, false);
+					return RedirectToAction("Index", "Senhas");
+				}
+				else
+				{
+					ModelState.AddModelError("", "*Insira um login e uma senha válida!");
+				}
+			}
+
+			return View();
+		}
+
+		[Authorize]
+		public ActionResult Deslogar()
+		{
+			Session.RemoveAll();
+			FormsAuthentication.SignOut();
+			return RedirectToAction("Login", "Conta");
+		}
+
+		[AllowAnonymous]
+		public ActionResult Cadastro()
+		{
+			if (User.Identity.IsAuthenticated)
+			{
+				return RedirectToAction("Index", "Senhas");
+			}
+			return View();
+		}
+
+		[HttpPost]
+		[AllowAnonymous]
+		public ActionResult Cadastro(Usuario usuario)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View();
+			}
+			using (GerenciaSenhasEntities bd = new GerenciaSenhasEntities())
+			{
+				var v = bd.Usuario.Where(a => a.Login.Equals(usuario.Login) && a.Senha.Equals(usuario.Senha)).FirstOrDefault();
+				if (v != null)
+				{
+					ModelState.AddModelError("", "*Usuário já cadastrado!");
+				}
+				else
+				{
+					Response.Write("<script>alert('Cadastro efetuado com sucesso');</script>");
+					bd.Usuario.Add(usuario);
+					bd.SaveChanges();
+					return RedirectToAction("Login", "Conta");
+				}
+			}
+			return View();
+		}
+	}
+}
---
+++ GerenciarSenhas/Controllers/SenhasController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using GerenciarSenhas.Models;
+
+namespace GerenciarSenhas.Controllers
+{
+	public class SenhasController : Controller
+    {
+        private GerenciaSenhasEntities db = new GerenciaSenhasEntities();
+
+		// GET: teste
+		[Authorize]
+		public ActionResult Index()
+        {
+            var senha = db.Senha.Include(s => s.Usuario);
+            return View(senha.ToList());
+        }
+
+		// GET: teste/Create
+		[Authorize]
+		public ActionResult Create()
+        {
+			ViewBag.IDUsuario = new SelectList(db.Usuario, "ID", "Login");
+            return View();
+        }
+
+        // POST: teste/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+		[Authorize]
+		public ActionResult Create([Bind(Include = "ID,IDUsuario,Senha1,LocalS,LoginS")] Senha senha)
+        {
+			senha.IDUsuario = Convert.ToInt32( Session["ID_usuario"]);
+			if (ModelState.IsValid)
+            {
+                db.Senha.Add(senha);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IDUsuario = new SelectList(db.Usuario, "ID", "Login", senha.IDUsuario);
+            return View(senha);
+        }
+
+		// GET: teste/Edit/5
+		[Authorize]
+		public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Senha senha = db.Senha.Find(id);
+            if (senha == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IDUsuario = new SelectList(db.Usuario, "ID", "Login", senha.IDUsuario);
+            return View(senha);
+        }
+
+        // POST: teste/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+		[Authorize]
+		public ActionResult Edit([Bind(Include = "ID,IDUsuario,Senha1,LocalS,LoginS")] Senha senha)
+        {
+			if( senha.LocalS == null || senha.LoginS == null || senha.Senha1 == null)
+			{
+				//ModelState.AddModelError("", "Insira todos os dados!");
+			}
+            if (ModelState.IsValid)
+            {
+                db.Entry(senha).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IDUsuario = new SelectList(db.Usuario, "ID", "Login", senha.IDUsuario);
+            return View(senha);
+        }
+
+		// GET: teste/Delete/5
+		[Authorize]
+		public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Senha senha = db.Senha.Find(id);
+            if (senha == null)
+            {
+                return HttpNotFound();
+            }
+            return View(senha);
+        }
+
+        // POST: teste/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+		[Authorize]
+		public ActionResult DeleteConfirmed(int id)
+        {
+            Senha senha = db.Senha.Find(id);
+            db.Senha.Remove(senha);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
